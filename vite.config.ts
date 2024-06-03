8680ca27fd291e337dc0e4d4d46258b78fdffe9b
HBilal Khan Yousafzai <hbilal_9@hotmail.com>
2024-06-03 11:48:47 +0500

    vite-plugin-dts used for types generation, vue version updated and types added

--- vite.config.ts
+++ vite.config.ts
@@ -1,45 +1,42 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import typescript2 from 'rollup-plugin-typescript2'
-import { fileURLToPath, URL } from 'url'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import dts from "vite-plugin-dts";
+import { fileURLToPath, URL } from "url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    typescript2({
-      check: false,
-      include: ["src/lib/*.vue"],
-      tsconfigOverride: {
-        compilerOptions: {
-          sourceMap: true,
-          declaration: true,
-          declarationMap: true
-        },
-        exclude: [ "vite.config.ts", "main.ts" ]
-      },
-    })
-  ],
-  build: {
-    cssCodeSplit: false,
-    lib: {
-      entry: "./src/StarRatings.ts",
-      formats: ['es', 'cjs'],
-      name: "StarRatings",
-      fileName: (format) => (format === "es" ? "index.js" : "index.cjs")
-    },
-    rollupOptions: {
-      external: ["vue"],
-      output: {
-        globals: {
-          vue: "Vue"
-        }
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+	plugins: [
+		vue(),
+		dts({
+			tsconfigPath: "./tsconfig.json",
+			include: ["src/**/*.vue", "src/**/*.ts"],
+			exclude: ["vite.config.ts", "main.ts"],
+			outDir: "dist/types",
+			insertTypesEntry: true,
+			rollupTypes: true,
+			entryRoot: "src",
+		}),
+	],
+	build: {
+		cssCodeSplit: false,
+		lib: {
+			entry: "./src/StarRatings.ts",
+			formats: ["es", "cjs"],
+			name: "StarRatings",
+			fileName: (format) => (format === "es" ? "index.js" : "index.cjs"),
+		},
+		rollupOptions: {
+			external: ["vue"],
+			output: {
+				globals: {
+					vue: "Vue",
+				},
+			},
+		},
+	},
+	resolve: {
+		alias: {
+			"@": fileURLToPath(new URL("./src", import.meta.url)),
+		},
+	},
+});
